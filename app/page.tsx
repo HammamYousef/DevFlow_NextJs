c11068ff4d4f6f38a97fa37bc07561f69f589d25
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-06-11 14:23:30 +0300

    Navbar structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">
-        Welcome to Next.js
-      </h1>
-      <h1 className="h1-bold font-inter">Welcome to Next.js (inter)</h1>
+      
     </div>
   );
 }
