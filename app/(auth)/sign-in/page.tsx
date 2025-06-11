45de348c5b893cb92955408b0d282cfce7955a1b
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-06-11 17:56:50 +0300

    Build the sign in\up form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,21 @@
+'use client';
+
+import AuthForm from '@/components/forms/AuthForm'
+import { SignInSchema } from '@/lib/validation'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <AuthForm
+      formType='SIGN_IN'
+      schema={SignInSchema}
+      defaultValues={{
+        email: '',
+        password: '',
+      }}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onSubmit={(data: any) => Promise.resolve({success: true, data})}
+    />
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import AuthForm from '@/components/forms/AuthForm'
+import { SignUpSchema } from '@/lib/validation'
 import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <AuthForm
+      formType='SIGN_UP'
+      schema={SignUpSchema}
+      defaultValues={{
+        email: '',
+        password: '',
+        name: '',
+        username: '',
+      }}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onSubmit={(data: any) => Promise.resolve({success: true, data})}
+    />
   )
 }
 
