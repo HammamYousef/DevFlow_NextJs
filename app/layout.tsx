2b3a5a746c95dd29e6f9e6159a7d44cf2ca08b5e
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-06-11 14:32:33 +0300

    Setup the theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Navbar from "./components/navigation/navbar";
+import Navbar from "../components/navigation/navbar";
 
 const inter = localFont({
   src: './fonts/InterVF.ttf',
