25237231f1117fc5a6ebfdcf4cf893e011769974
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-06-14 18:55:56 +0300

    Implement Question and Answer Models

---
+++ database/answer.model.ts
@@ -0,0 +1,33 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAnswer {
+  content: string;
+  questionId: Types.ObjectId;
+  authorId: Types.ObjectId;
+  votes?: {
+    upvotes?: number;
+    downvotes?: number;
+  };
+}
+
+const AnswerSchema = new Schema<IAnswer>(
+  {
+    content: { type: String, required: true },
+    questionId: {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+    authorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    votes: {
+      upvotes: { type: Number, default: 0 },
+      downvotes: { type: Number, default: 0 },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Answer = models?.answer || model<IAnswer>("Answer", AnswerSchema);
+export default Answer;
---
+++ database/question.model.ts
@@ -0,0 +1,39 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  title: string;
+  content: string;
+  tags: Types.ObjectId[];
+  authorId: Types.ObjectId;
+  answers?: number;
+  views?: number;
+  votes?: {
+    upvotes?: number;
+    downvotes?: number;
+  };
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    authorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    answers: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
+    votes: {
+      type: {
+        upvotes: { type: Number, default: 0 },
+        downvotes: { type: Number, default: 0 },
+      },
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Question =
+  models?.question || model<IQuestion>("Question", QuestionSchema);
+export default Question;
