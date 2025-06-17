976f34c156399e33a8bafbb2c03c255dba249ff2
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-06-18 00:21:23 +0300

    Implement API Fetch Handler

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -1,14 +1,17 @@
 import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import { AccountSchema } from "@/lib/validation";
 import { APIErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
 
+// This route handles POST requests to fetch an account by its providerAccountId.
 export async function POST(request: Request) {
   const { providerAccountId } = await request.json();
 
   try {
+    await dbConnect();
     const validateData = AccountSchema.partial().safeParse({
       providerAccountId,
     });
--- app/api/users/email/route.ts
+++ app/api/users/email/route.ts
@@ -1,14 +1,17 @@
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import { UserSchema } from "@/lib/validation";
 import { APIErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
 
+// This route handles POST requests to fetch a user by their email.
 export async function POST(request: Request) {
   const { email } = await request.json();
 
   try {
+    await dbConnect();
     const validateData = UserSchema.partial().safeParse({ email });
     if (!validateData.success) {
       throw new ValidationError(validateData.error.flatten().fieldErrors);
